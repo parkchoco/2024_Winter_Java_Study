b9288b1f82b8e829393fdf108b2ccb8e8ac776b9
parkchoco <155618728+parkchoco@users.noreply.github.com>
2024-01-11 17:29:22 +0900

    Java Programming 03장 프로젝트

---
+++ Study_Java_Chap03/src/chap03/Project_05.java
@@ -0,0 +1,23 @@
+package chap03;
+
+import java.util.Scanner;
+
+public class Project_05{
+	public static void main(String args[]){
+		Scanner input = new Scanner(System.in);
+		int sum = 0;
+		System.out.println("정수를 5개 입력하세요");
+		
+		for(int i = 0;i < 5;i++){
+			int num = input.nextInt();
+			if(num>0){
+				sum+=num;
+			}
+			else{
+				continue;
+			}
+		}
+		System.out.println("양수의 합은 " + sum);
+		input.close();
+	}
+}

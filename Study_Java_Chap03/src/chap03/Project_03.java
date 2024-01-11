dc46daf65be64fd97a8b1aad0366f118301f08f5
parkchoco <psy0881@naver.com>
2024-01-11 09:44:26 +0900

    Java Programming 04장 프로젝트

---
+++ Study_Java_Chap03/src/chap03/Project_03.java
@@ -0,0 +1,12 @@
+package chap03;
+
+public class Project_03 {
+	public static void main(String args[]) {
+		char c = 'a';
+		
+		do {
+			System.out.print(c);
+			c=(char)(c + 1);
+		}while(c <= 'z');
+	}
+}

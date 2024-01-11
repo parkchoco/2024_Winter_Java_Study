60af811a3ff3a6ffe1b0b6557eb86a2a4f0ea1b1
parkchoco <psy0881@naver.com>
2024-01-11 09:10:58 +0900

    Java Programmin 04장 프로젝트

--- Study_Java_Chap04/src/chap04/Project_01.java
+++ Study_Java_Chap04/src/chap04/Project_01.java
@@ -3,7 +3,7 @@ package chap04;
 public class Project_01 {
 
 	public static void main(String[] args) {
-		System.out.println("æ»≥Á«œººø‰");
+		System.out.println("æ»≥Á«œººø‰!!");
 
 	}
 
--- Study_Java_Chap04/src/chap04/Project_02.java
+++ Study_Java_Chap04/src/chap04/Project_02.java
@@ -3,7 +3,7 @@ package chap04;
 public class Project_02 {
 
 	public static void main(String[] args) {
-		System.out.println("æ»≥Á«œººø‰zz");
+		System.out.println("æ»≥Á«œººø‰!!!!!");
 
 	}
 
